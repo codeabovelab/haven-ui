1d06bb169acbdb378ae4a68136e56afdcaaa2240
sklyukin <stas.msu@gmail.com>
2016-04-26 11:11:18 +0500

    general layout improved

--- src/containers/ClusterDetail/ClusterDetail.js
+++ src/containers/ClusterDetail/ClusterDetail.js
@@ -28,7 +28,6 @@ export default class ClusterDetail extends Component {
 
 
   render() {
-    const s = require('./ClusterDetail.scss');
     const {containers, params: {name}, deleteCluster} = this.props; // eslint-disable-line no-shadow
 
     function handleDelete() {
@@ -40,60 +39,56 @@ export default class ClusterDetail extends Component {
 
 
     return (
-      <div className={s.clusterDetail}>
-        <div className="container-fluid">
-          <h1>
-            <Link to="/clusters">Clusters</Link> / {name}
-          </h1>
-          <div className="page-info-group">
-            # of Containers: <strong>{containers && containers.length}</strong>
-          </div>
-          <div className="page-actions">
-            <div className="btn-group">
-              <button className="btn btn-primary" disabled><i className="fa fa-plus"></i> New Container</button>
-              <button className="btn btn-danger" onClick={handleDelete}><i className="fa fa-trash"></i> Delete Cluster
-              </button>
-            </div>
-          </div>
-          <div className="clearfix">
-
-          </div>
-          {containers && containers.length > 0 &&
-          <div>
-            <h2>Containers</h2>
-            <div className="table-responsive">
-              <table className="table table-striped table-sm">
-                <thead>
-                <tr>
-                  <th>Name</th>
-                  <th>Image Name</th>
-                  <th>Node Name</th>
-                  <th>Port Mapping</th>
-                  <th>Status</th>
-                  <th>Actions</th>
-                </tr>
-                </thead>
-                <tbody>
-                {containers.map(container =>
-                  <tr key={container.name}>
-                    <td>{container.name}</td>
-                    <td>{container.image}</td>
-                    <td>{container.node}</td>
-                    <td>{container.ports}</td>
-                    <td>{container.status}</td>
-                    <td></td>
-                  </tr>
-                )}
-                </tbody>
-              </table>
-            </div>
-          </div>
-          }
-          {containers && containers.length === 0 &&
-          <div className="alert alert-info">
-            No containers yet
-          </div>}
+      <div className="container-fluid">
+        <h1>
+          <Link to="/clusters">Clusters</Link> / {name}
+        </h1>
+        <div className="page-info-group">
+          # of Containers: <strong>{containers && containers.length}</strong>
         </div>
+        <div className="page-actions">
+          <div className="btn-group">
+            <button className="btn btn-primary" disabled><i className="fa fa-plus"></i> New Container</button>
+            <button className="btn btn-danger" onClick={handleDelete}><i className="fa fa-trash"></i> Delete Cluster
+            </button>
+          </div>
+        </div>
+        <div className="clearfix"></div>
+        {containers && containers.length > 0 &&
+        <div>
+          <h2>Containers</h2>
+          <div className="table-responsive">
+            <table className="table table-striped table-sm">
+              <thead>
+              <tr>
+                <th>Name</th>
+                <th>Image Name</th>
+                <th>Node Name</th>
+                <th>Port Mapping</th>
+                <th>Status</th>
+                <th>Actions</th>
+              </tr>
+              </thead>
+              <tbody>
+              {containers.map(container =>
+                <tr key={container.name}>
+                  <td>{container.name}</td>
+                  <td>{container.image}</td>
+                  <td>{container.node}</td>
+                  <td>{container.ports}</td>
+                  <td>{container.status}</td>
+                  <td></td>
+                </tr>
+              )}
+              </tbody>
+            </table>
+          </div>
+        </div>
+        }
+        {containers && containers.length === 0 &&
+        <div className="alert alert-info">
+          No containers yet
+        </div>}
       </div>
     );
   }
