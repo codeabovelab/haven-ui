55af6e6f117bd0346990f7890de4560f54c2fe87
sklyukin <stas.msu@gmail.com>
2016-05-11 16:29:38 +0500

    disable server-side rendering

--- src/server.js
+++ src/server.js
@@ -75,37 +75,7 @@ app.use((req, res) => {
       ReactDOM.renderToString(<Html assets={webpackIsomorphicTools.assets()} store={store}/>));
   }
 
-  if (__DISABLE_SSR__) {
-    hydrateOnClient();
-    return;
-  }
-
-  match({ history, routes: getRoutes(store), location: req.originalUrl }, (error, redirectLocation, renderProps) => {
-    if (redirectLocation) {
-      res.redirect(redirectLocation.pathname + redirectLocation.search);
-    } else if (error) {
-      console.error('ROUTER ERROR:', pretty.render(error));
-      res.status(500);
-      hydrateOnClient();
-    } else if (renderProps) {
-      loadOnServer({...renderProps, store, helpers: {client}}).then(() => {
-        const component = (
-          <Provider store={store} key="provider">
-            <ReduxAsyncConnect {...renderProps} />
-          </Provider>
-        );
-
-        res.status(200);
-
-        global.navigator = {userAgent: req.headers['user-agent']};
-
-        res.send('<!doctype html>\n' +
-          ReactDOM.renderToString(<Html assets={webpackIsomorphicTools.assets()} component={component} store={store}/>));
-      });
-    } else {
-      res.status(404).send('Not found');
-    }
-  });
+  hydrateOnClient();
 });
 
 if (config.port) {
