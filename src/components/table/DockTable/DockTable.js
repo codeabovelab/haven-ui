d9926edd644c0e62d2f37ecf43763a1038e98ce0
sklyukin <stas.msu@gmail.com>
2016-05-25 23:45:54 +0500

    filtering

--- src/components/table/DockTable/DockTable.js
+++ src/components/table/DockTable/DockTable.js
@@ -11,15 +11,18 @@ export default class DockTable extends Component {
 
 
   //general
+  total = 0; // before filtering
+  pagesTotal = 0; // before filtering
   groupByColumn;
   pageSize = 10;
 
+
   //after filtering and sorting
   query = "";
+  filteredTotal = 0;
   groups;
   columns;
-  pageTotal = 1;
-  total = 0;
+  pagesNumber = 1;
 
   //for current page
   currentGroups = [];
@@ -34,9 +37,11 @@ export default class DockTable extends Component {
       query: ""
     };
 
-    const {groupBy, columns} = this.props;
+    const {rows, groupBy, columns} = this.props;
     this.columns = columns.filter(column => column.name !== groupBy);
     this.groupByColumn = columns.find(column => column.name === groupBy);
+    this.total = rows.length;
+    this.pagesTotal = Math.max(1, Math.ceil(this.total / this.pageSize));
     this.applyFilteringAndSorting();
   }
 
@@ -116,15 +121,18 @@ export default class DockTable extends Component {
   }
 
   applyFilteringAndSorting() {
-    const {rows, groupBy} = this.props;
-    this.total = rows.length;
+    const {rows, groupBy, columns} = this.props;
+    let columnNames = columns.map(column => column.name);
+    this.query = this.state.query;
+    let filteredRows = this.filterRows(this.query, rows, columnNames);
+    this.filteredTotal = filteredRows.length;
     let groups = null;
     if (groupBy) {
-      groups = _.groupBy(rows, groupBy);
+      groups = _.groupBy(filteredRows, groupBy);
       groups = _.mapValues(groups, (rows, key) => ({rows: rows, opened: true, key: key}));
     }
     this.groups = groups;
-    this.pageTotal = Math.max(1, Math.ceil(rows.length / this.pageSize));
+    this.pagesNumber = Math.max(1, Math.ceil(filteredRows.length / this.pageSize));
     this.generateCurrentGroups();
   }
 
@@ -203,12 +211,12 @@ export default class DockTable extends Component {
     const {pageCurrent} = this.state;
     const MAX_PAGES_LINKS = 5;
 
-    if (this.pageTotal <= 1) {
+    if (this.pagesTotal <= 1) {
       return <div></div>;
     }
     let from = (pageCurrent - 1) * this.pageSize + 1;
-    let to = Math.min(pageCurrent * this.pageSize, this.total);
-    let pagesLinks = Math.min(this.pageTotal, MAX_PAGES_LINKS);
+    let to = Math.min(pageCurrent * this.pageSize, this.filteredTotal);
+    let pagesLinks = Math.min(this.pagesNumber, MAX_PAGES_LINKS);
 
     let pagesNumbers = new Array(pagesLinks); // 3, 4, 5, 6, 7
     //Math.floor((pagesLinks - 1) / 2) - left center eq to this.page.Current
@@ -217,7 +225,7 @@ export default class DockTable extends Component {
       pagesNumbers[i] = startPage + i;
     }
 
-    pagesNumbers = pagesNumbers.filter(pageNumber => pageNumber <= this.pageTotal);
+    pagesNumbers = pagesNumbers.filter(pageNumber => pageNumber <= this.pagesNumber);
     while ((pagesNumbers.length < MAX_PAGES_LINKS) && (pagesNumbers[0] > 1)) {
       //if array is small like 5,6,7 - let's add few more links at start
       pagesNumbers.unshift(pagesNumbers[0] - 1);
@@ -225,7 +233,10 @@ export default class DockTable extends Component {
 
     return (
       <div className="pagination-wrapper">
-        <div className="pagination-showing text-muted">Showing {from} to {to} of {this.total} entries</div>
+        <div className="pagination-showing text-muted">
+          {pagesNumbers.length > 1 && <span>Showing {from} to {to} of {this.filteredTotal} entries</span>}
+          {pagesNumbers.length === 1 && <span>Showing {this.filteredTotal} entries</span>}
+        </div>
         <nav>
           <ul className="pagination">
             <li className={"page-item " + (pageCurrent === 1 ? 'disabled' : '')}>
@@ -241,7 +252,7 @@ export default class DockTable extends Component {
                 </li>
               );
             })}
-            <li className={"page-item " + (pageCurrent === this.pageTotal ? 'disabled' : '')}>
+            <li className={"page-item " + (pageCurrent === this.pagesNumber ? 'disabled' : '')}>
               <a className="page-link" onClick={this.changePage.bind(this, pageCurrent + 1)} aria-label="Next">
                 <span aria-hidden="true">&raquo;</span>
                 <span className="sr-only">Next</span>
@@ -254,7 +265,7 @@ export default class DockTable extends Component {
   }
 
   changePage(pageNumber) {
-    if ((pageNumber < 1) || (this.state.pageCurrent === pageNumber) || (pageNumber > this.pageTotal)) {
+    if ((pageNumber < 1) || (this.state.pageCurrent === pageNumber) || (pageNumber > this.pagesNumber)) {
       return;
     }
 
@@ -266,6 +277,31 @@ export default class DockTable extends Component {
   }
 
   queryChange(e) {
-    console.log('query changed', e.target.value);
+    let query = e.target.value;
+    this.setState({
+      ...this.state,
+      query
+    });
+  }
+
+  filterRows(query, rows, columnsNames) {
+    if (!query) {
+      return rows;
+    }
+    let q = query.toLowerCase();
+
+    return rows.filter(row => {
+      let data = _.pick(row, columnsNames);
+      for (let fieldName in data) {
+        if (data.hasOwnProperty(fieldName)) {
+          let fieldValue = data[fieldName];
+          if (fieldValue != null) {
+            fieldValue = fieldValue.toString().toLowerCase();
+            if (fieldValue.includes(q)) return true;
+          }
+        }
+      }
+      return false;
+    });
   }
 }
