dc455b8a9ab199bfc23fdad7f75d16bf521c2eeb
sklyukin <stas.msu@gmail.com>
2016-05-26 11:28:15 +0500

    order columns

--- src/components/table/DockTable/DockTable.js
+++ src/components/table/DockTable/DockTable.js
@@ -19,41 +19,52 @@ export default class DockTable extends Component {
 
 
   //after filtering and sorting
-  query = "";
+  query = ""; //last query
+  sorting = ""; //last sorting
   filteredTotal = 0;
-  groups;
-  columns;
+  groups; //either groups or sortedRows should be used for displaying data
+  sortedRows;
+  columns;//Array columns without groupBy
+  allColumns;// with groupBy
   pagesNumber = 1;
+
 
   //for current page
   currentGroups = [];
+  currentRows = []; // for displaying data without groups
+  currentPage = 1;
 
   //noinspection JSDuplicatedDeclaration
   constructor(...params) {
     super(...params);
     this.state = {
-      pageCurrent: 1,
+      currentPage: 1,
       groupsState: {},
       closedGroups: {},
       sortingColumn: "",
-      sortingOrder: 'ASC',
+      sortingOrder: 'asc',
       query: ""
     };
 
     const {rows, groupBy, columns} = this.props;
     this.columns = columns.filter(column => column.name !== groupBy);
     this.groupByColumn = columns.find(column => column.name === groupBy);
+    this.allColumns = [this.groupByColumn].concat(this.columns);
     this.total = rows.length;
     this.pagesTotal = Math.max(1, Math.ceil(this.total / this.pageSize));
     this.applyFilteringAndSorting();
   }
 
   render() {
-    if (this.query !== this.state.query) {
+    if ((this.query !== this.state.query) ||
+      (this.sorting !== `${this.state.sortingColumn} ${this.state.sortingOrder}`)) {
       this.applyFilteringAndSorting();
     }
+    if (this.currentPage !== this.state.currentPage) {
+      this.generateCurrentPageData();
+    }
     const s = require('./DockTable.scss');
-    const {groupBy, title} = this.props;
+    const {title} = this.props;
     return (
       <div className={s.dockTable}>
         <div className="docktable-header">
@@ -62,15 +73,16 @@ export default class DockTable extends Component {
                  placeholder="Search"/>
         </div>
         <div className="table-responsive">
-          {groupBy && this.groupsRender()}
+          {this.groups && this.renderGroups()}
+          {this.sortedRows && this.renderNoGroups()}
         </div>
         {this.renderPagination()}
       </div>
     );
   }
 
-  groupsRender() {
-    const {sortingColumn, sortingOrder, closedGroups} = this.state;
+  renderGroups() {
+    const {closedGroups} = this.state;
     const groupEls = [];
     let columns = this.columns;
     this.currentGroups.forEach(group => {
@@ -110,57 +122,82 @@ export default class DockTable extends Component {
 
     return (
       <table className="table table-bordered table-striped table-sm">
-        <thead>
-        <tr>
-          {this.groupByColumn && <th>{DockTable.columnLabel(this.groupByColumn)}</th>}
-          {columns && columns.map(column => (
-            <th key={column.name} className={column.sortable ? 'sortable' : ''}
-            onClick={this.toggleSorting.bind(this, column.name)}>
-              {DockTable.columnLabel(column)}
-              {column.sortable && (<span className="sorting">
-                {sortingColumn !== column.name && <i className="fa fa-sort"/>}
-                {sortingColumn === column.name && (
-                  <span>
-                    {sortingOrder === 'ASC' && <i className="fa fa-sort-asc"/>}
-                    {sortingOrder === 'DESC' && <i className="fa fa-sort-desc"/>}
-                  </span>
-                )}
-              </span>)}
-            </th>
-          ))}
-        </tr>
-        </thead>
+        {this.renderHeader()}
         {groupEls}
       </table>
     );
   }
 
+  renderNoGroups() {
+    const columns = this.allColumns;
+    let emptyTrsNumber = 0;
+    if (this.currentPage !== 1) {
+      emptyTrsNumber = this.pageSize - this.currentRows.length;
+    }
+    let emptyTrs = new Array(emptyTrsNumber);
+    emptyTrs.fill(1);
+    return (
+      <table className="table table-bordered table-striped table-sm">
+        {this.renderHeader()}
+        <tbody>
+        {this.currentRows.map((model, i) => (
+          <tr key={i}>
+            {columns.map(column => DockTable.tdRender(column.name, model))}
+          </tr>
+        ))}
+        {emptyTrs.map((value, i) => (
+          <tr key={i}>{columns.map(column => <td key={column.name}>&nbsp;</td>)}</tr>
+        ))}
+        </tbody>
+      </table>
+    );
+  }
+
   applyFilteringAndSorting() {
+    this.query = this.state.query;
+    const {sortingColumn, sortingOrder} = this.state;
+    this.sorting = `${sortingColumn} ${sortingOrder}`;
     const {rows, groupBy, columns} = this.props;
     let columnNames = columns.map(column => column.name);
-    this.query = this.state.query;
     let filteredRows = this.filterRows(this.query, rows, columnNames);
     this.filteredTotal = filteredRows.length;
-    let groups = null;
-    if (groupBy) {
+    this.pagesNumber = Math.max(1, Math.ceil(this.filteredTotal / this.pageSize));
+
+    let groups;
+    let sortedRows;
+    if (groupBy && !sortingColumn) {
       groups = _.groupBy(filteredRows, groupBy);
       groups = _.mapValues(groups, (rows, key) => ({rows: rows, opened: true, key: key}));
+    } else {
+      sortedRows = sortingColumn ? _.orderBy(filteredRows, sortingColumn, sortingOrder) : filteredRows;
     }
     this.groups = groups;
-    this.pagesNumber = Math.max(1, Math.ceil(filteredRows.length / this.pageSize));
+    this.sortedRows = sortedRows;
+
+    this.generateCurrentPageData();
+  }
+
+  generateCurrentPageData() {
+    this.currentPage = this.state.currentPage;
     this.generateCurrentGroups();
+    this.generateCurrentRows();
   }
 
   generateCurrentGroups(page = null) {
-    const pageCurrent = page ? page : this.state.pageCurrent;
+    if (!this.groups) {
+      //so it is simple rows mode
+      this.currentGroups = null;
+      return;
+    }
     this.currentGroups = [];
     if (this.groups.length === 0) {
       return;
     }
+    const currentPage = page ? page : this.state.currentPage;
     let groups = [];
     //to save order of properties use _.forOwn instead _.values
     _.forOwn(this.groups, (group, key) => groups.push(group));
-    let toSkip = this.pageSize * (pageCurrent - 1);
+    let toSkip = this.pageSize * (currentPage - 1);
     let skipped = 0;
     let i = 0;
     //wholes group to skip
@@ -195,14 +232,23 @@ export default class DockTable extends Component {
     }
   }
 
+  generateCurrentRows() {
+    if (!this.sortedRows) {
+      this.currentRows = null;
+      return;
+    }
+    let from = (this.state.currentPage - 1) * this.pageSize;
+    let to = this.state.currentPage * this.pageSize;
+    this.currentRows = this.sortedRows.slice(from, to);
+  }
+
   toggleSorting(columnName) {
     let {sortingColumn, sortingOrder} = this.state;
     if (sortingColumn !== columnName) {
-      console.log('not equal', sortingColumn, columnName);
       sortingColumn = columnName;
-      sortingOrder = 'ASC';
+      sortingOrder = 'asc';
     } else {
-      sortingOrder = sortingOrder === 'ASC' ? 'DESC' : 'ASC';
+      sortingOrder = sortingOrder === 'asc' ? 'desc' : 'asc';
     }
 
     this.setState({...this.state, sortingColumn, sortingOrder});
@@ -236,19 +282,19 @@ export default class DockTable extends Component {
   }
 
   renderPagination() {
-    const {pageCurrent} = this.state;
+    const {currentPage} = this.state;
     const MAX_PAGES_LINKS = 5;
 
     if (this.pagesTotal <= 1) {
       return <div></div>;
     }
-    let from = (pageCurrent - 1) * this.pageSize + 1;
-    let to = Math.min(pageCurrent * this.pageSize, this.filteredTotal);
+    let from = (currentPage - 1) * this.pageSize + 1;
+    let to = Math.min(currentPage * this.pageSize, this.filteredTotal);
     let pagesLinks = Math.min(this.pagesNumber, MAX_PAGES_LINKS);
 
     let pagesNumbers = new Array(pagesLinks); // 3, 4, 5, 6, 7
     //Math.floor((pagesLinks - 1) / 2) - left center eq to this.page.Current
-    let startPage = Math.max(1, pageCurrent - Math.floor((pagesLinks - 1) / 2));
+    let startPage = Math.max(1, currentPage - Math.floor((pagesLinks - 1) / 2));
     for (let i = 0; i < pagesLinks; i++) {
       pagesNumbers[i] = startPage + i;
     }
@@ -267,21 +313,21 @@ export default class DockTable extends Component {
         </div>
         <nav>
           <ul className="pagination">
-            <li className={"page-item " + (pageCurrent === 1 ? 'disabled' : '')}>
-              <a className="page-link" onClick={this.changePage.bind(this, pageCurrent - 1)} aria-label="Previous">
+            <li className={"page-item " + (currentPage === 1 ? 'disabled' : '')}>
+              <a className="page-link" onClick={this.changePage.bind(this, currentPage - 1)} aria-label="Previous">
                 <span aria-hidden="true">&laquo;</span>
                 <span className="sr-only">Previous</span>
               </a>
             </li>
             {pagesNumbers.map(pageNumber => {
               return (
-                <li className={"page-item " + (pageCurrent === pageNumber ? 'active' : '')} key={pageNumber}>
+                <li className={"page-item " + (currentPage === pageNumber ? 'active' : '')} key={pageNumber}>
                   <a className="page-link" onClick={this.changePage.bind(this, pageNumber)}>{pageNumber}</a>
                 </li>
               );
             })}
-            <li className={"page-item " + (pageCurrent === this.pagesNumber ? 'disabled' : '')}>
-              <a className="page-link" onClick={this.changePage.bind(this, pageCurrent + 1)} aria-label="Next">
+            <li className={"page-item " + (currentPage === this.pagesNumber ? 'disabled' : '')}>
+              <a className="page-link" onClick={this.changePage.bind(this, currentPage + 1)} aria-label="Next">
                 <span aria-hidden="true">&raquo;</span>
                 <span className="sr-only">Next</span>
               </a>
@@ -293,15 +339,14 @@ export default class DockTable extends Component {
   }
 
   changePage(pageNumber) {
-    if ((pageNumber < 1) || (this.state.pageCurrent === pageNumber) || (pageNumber > this.pagesNumber)) {
+    if ((pageNumber < 1) || (this.state.currentPage === pageNumber) || (pageNumber > this.pagesNumber)) {
       return;
     }
 
     this.setState({
       ...this.state,
-      pageCurrent: pageNumber
+      currentPage: pageNumber
     });
-    this.generateCurrentGroups(pageNumber);
   }
 
   queryChange(e) {
@@ -331,5 +376,31 @@ export default class DockTable extends Component {
       }
       return false;
     });
+  }
+
+  renderHeader() {
+    const {sortingColumn, sortingOrder} = this.state;
+    return (
+      <thead>
+      <tr>
+        {this.groupByColumn && <th>{DockTable.columnLabel(this.groupByColumn)}</th>}
+        {this.columns && this.columns.map(column => (
+          <th key={column.name} className={column.sortable ? 'sortable' : ''}
+              onClick={column.sortable && this.toggleSorting.bind(this, column.name)}>
+            {DockTable.columnLabel(column)}
+            {column.sortable && (<span className="sorting">
+                {sortingColumn !== column.name && <i className="fa fa-sort"/>}
+              {sortingColumn === column.name && (
+                <span>
+                    {sortingOrder === 'asc' && <i className="fa fa-sort-asc"/>}
+                  {sortingOrder === 'desc' && <i className="fa fa-sort-desc"/>}
+                  </span>
+              )}
+              </span>)}
+          </th>
+        ))}
+      </tr>
+      </thead>
+    );
   }
 }
