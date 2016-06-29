6b1f6f37d8d87fede3da395ec0c72594df3fe18a
Andrey Miroshnichenko <a@avm.name>
2016-06-29 22:18:48 +0300

    Hide left navigation for non-authenticated users
    Fixes #DM-63

--- src/components/MenuLeft/MenuLeft.js
+++ src/components/MenuLeft/MenuLeft.js
@@ -4,55 +4,66 @@ import {toggle} from 'redux/modules/menuLeft/menuLeft';
 import { connect } from 'react-redux';
 
 @connect(
-  state => ({toggled: state.menuLeft.toggled}),
+  state => ({toggled: state.menuLeft.toggled, user: state.auth.user}),
   {toggle}
 )
 export default class MenuLeft extends Component {
   static propTypes = {
     toggled: PropTypes.bool,
-    toggle: PropTypes.func.isRequired
+    toggle: PropTypes.func.isRequired,
+    user: PropTypes.object
   };
 
   render() {
     const {toggled, toggle} = this.props;
-    return (
-      <div data-toggle={toggled} className="ml">
-        <div className="ml-header">
-          <span className="pull-xs-right hidden-xs-down" onClick={toggle}><i className="fa fa-bars"/></span>
-        </div>
-        <div className="nav nav-pills nav-stacked">
-          <li className="nav-item" title="Dashboard">
-            <Link to="/dashboard" className="nav-link">
-              <i className="fa fa-tachometer fa-fw"/>
-              <span className="label">Dashboard</span>
-            </Link>
-          </li>
-          <li className="nav-item" title="Clusters">
-            <Link to="/clusters" className="nav-link">
-              <i className="fa fa-object-group fa-fw"/>
-              <span className="label">Cluster List</span>
-            </Link>
-          </li>
-          <li className="nav-item" title="Nodes">
-            <Link to="/nodes" className="nav-link">
-              <i className="fa fa-server fa-fw"/>
-              <span className="label">Node List</span>
-            </Link>
-          </li>
-          <li className="nav-item" title="Images">
-            <Link to="/images" className="nav-link">
-              <i className="fa fa-file-o fa-fw"/>
-              <span className="label">Image List</span>
-            </Link>
-          </li>
-          <li className="nav-item" title="Registries">
-            <Link to="/registries" className="nav-link">
-              <i className="fa fa-list fa-fw"/>
-              <span className="label">Registries List</span>
-            </Link>
-          </li>
-        </div>
-      </div>
+    console.log(this.props);
+
+    let sidemenu = (
+      <div />
     );
+
+    if (this.props.user) {
+      sidemenu = (
+        <div data-toggle={toggled} className="ml">
+          <div className="ml-header">
+            <span className="pull-xs-right hidden-xs-down" onClick={toggle}><i className="fa fa-bars"/></span>
+          </div>
+          <div className="nav nav-pills nav-stacked">
+            <li className="nav-item" title="Dashboard">
+              <Link to="/dashboard" className="nav-link">
+                <i className="fa fa-tachometer fa-fw"/>
+                <span className="label">Dashboard</span>
+              </Link>
+            </li>
+            <li className="nav-item" title="Clusters">
+              <Link to="/clusters" className="nav-link">
+                <i className="fa fa-object-group fa-fw"/>
+                <span className="label">Cluster List</span>
+              </Link>
+            </li>
+            <li className="nav-item" title="Nodes">
+              <Link to="/nodes" className="nav-link">
+                <i className="fa fa-server fa-fw"/>
+                <span className="label">Node List</span>
+              </Link>
+            </li>
+            <li className="nav-item" title="Images">
+              <Link to="/images" className="nav-link">
+                <i className="fa fa-file-o fa-fw"/>
+                <span className="label">Image List</span>
+              </Link>
+            </li>
+            <li className="nav-item" title="Registries">
+              <Link to="/registries" className="nav-link">
+                <i className="fa fa-list fa-fw"/>
+                <span className="label">Registries List</span>
+              </Link>
+            </li>
+          </div>
+        </div>
+      );
+    }
+
+    return sidemenu;
   }
 }
