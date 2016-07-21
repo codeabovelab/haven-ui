8ce881d9573c2c93a59be461828682655d7b793d
Andrey Miroshnichenko <a@avm.name>
2016-07-22 01:38:23 +0300

    Bug fixes

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -86,49 +86,5 @@ export default class Navbar extends Component {
         </Dropdown>
       </div>
     );
-
-    /*
-
-     <div className="user-profile clearfix">
-     <div className="al-user-profile" uib-dropdown>
-     <ul className="top-dropdown-menu profile-dropdown" uib-dropdown-menu>
-     <li><i className="dropdown-arr"></i></li>
-     <li><a href="#/profile"><i className="fa fa-user"></i>Profile</a></li>
-     <li><a href><i className="fa fa-cog"></i>Settings</a></li>
-     <li><a href className="signout"><i className="fa fa-power-off"></i>Sign out</a></li>
-     </ul>
-     </div>
-     </div>
-
-
-     <nav className={"navbar " + s.navbar}>
-     <ul className="nav navbar-nav">
-     <li className={"nav-item " + s["nav-item-github"]}>
-     <a className="nav-link" href="http://github.com" target="_blank" title="View on Github"><i
-     className="fa fa-github"/></a>
-     </li>
-     </ul>
-     <span className="brand-container">
-     <IndexLink to="/dashboard" className="navbar-brand">{config.app.title}</IndexLink>
-     </span>
-     <ul className="nav navbar-nav pull-xs-right">
-     {!user &&
-     <li className="nav-item">
-     <Link to="/login" className="nav-link"><i className="fa fa-sign-in"/> Login</Link>
-     </li>
-     }
-     {user &&
-     <li className="nav-item">
-     <span className="nav-link"><strong>{user.name}</strong></span>
-     </li>
-     }
-     {user &&
-     <li className="nav-item">
-     <a className="nav-link" onClick={this.handleLogout}><i className="fa fa-sign-in"/> Logout</a>
-     </li>
-     }
-     </ul>
-     </nav>
-     */
   }
 }
