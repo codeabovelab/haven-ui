e98716dc3f52297ffc01036c076ce1a119b12880
Yuriy Zhuk <zhukyuri@gmail.com>
2016-08-31 22:44:46 +0300

    DM-110 Added Action column with dropdown

--- src/components/nodes/NodesList.js
+++ src/components/nodes/NodesList.js
@@ -1,6 +1,6 @@
 import React, {Component, PropTypes} from 'react';
 import {Link} from 'react-router';
-import {DockTable} from '../index';
+import {DockTable, ActionMenu} from '../index';
 import {Label, Badge, ButtonToolbar, SplitButton, MenuItem, Panel, Button, ProgressBar, Glyphicon} from 'react-bootstrap';
 
 export default class NodesList extends Component {
@@ -35,15 +35,30 @@ export default class NodesList extends Component {
       label: 'Health Status',
       width: '20%',
       render: this.healthRender
+    },
+    {
+      name: 'actions',
+      label: 'Actions',
+      width: '20%'
+    }
+  ];
+
+  ACTIONS = [
+    {
+      key: "info",
+      title: "Info"
     }
   ];
 
   render() {
+    const {data: rows} = this.props;
     const panelHeader = (
       <div className="clearfix">
         <h3>Nodes List</h3>
       </div>
     );
+
+    this.additionalData(rows);
 
     return (
       <Panel header={panelHeader}>
@@ -52,11 +67,36 @@ export default class NodesList extends Component {
       )}
       {(this.props.data && !this.props.loading) && (
         <DockTable columns={this.COLUMNS}
-                    rows={this.props.data}
+                    rows={rows}
         />
       )}
       </Panel>
     );
+  }
+
+  additionalData(rows) {
+    if (rows) {
+      rows.forEach(row => {
+        row.__attributes = {'data-id': row.id};
+        row.actions = this.tdActions.bind(this);
+      });
+    }
+  }
+
+  tdActions(node) {
+    return (
+      <td className="td-actions" key="actions">
+        <ActionMenu subject={node.id}
+                    actions={this.ACTIONS}
+                    actionHandler={this.onActionInvoke.bind(this)}
+        />
+
+      </td>
+    );
+  }
+
+  onActionInvoke(action, nodes) {
+    console.log('onActionInvokeNodes', action, nodes);
   }
 
   healthRender(registry) {
