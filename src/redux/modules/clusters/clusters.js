728bf975a6217763c9cb1654cc640501b991003e
Alexander Turivnoi <a.turivnoi@yandex.ru>
2016-09-05 19:45:47 +0300

    saves cluster description when created

--- src/redux/modules/clusters/clusters.js
+++ src/redux/modules/clusters/clusters.js
@@ -73,10 +73,10 @@ export function load() {
   };
 }
 
-export function create(name) {
+export function create(name, data) {
   return {
     types: [ACTIONS.CREATE, ACTIONS.CREATE_SUCCESS, ACTIONS.CREATE_FAIL],
-    promise: (client) => client.put(`/ui/api/clusters/${name}`, {data: {}})
+    promise: (client) => client.put(`/ui/api/clusters/${name}`, {data: data})
   };
 }
 
