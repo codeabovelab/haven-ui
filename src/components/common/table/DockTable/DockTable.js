1a03def10854b77b1e2cc7bd5404a9fb26bb8a47
wayerr <radiofun@ya.ru>
2016-09-29 22:43:49 +0300

    DockTable: allow to use custom render for group headers (by workaround)

--- src/components/common/table/DockTable/DockTable.js
+++ src/components/common/table/DockTable/DockTable.js
@@ -371,7 +371,9 @@ export default class DockTable extends Component {
             <span className="group-title" onClick={this.toggleGroup.bind(this, group.key)}>
             {!closed && <i className="fa fa-minus"/>}
               {closed && <i className="fa fa-plus"/>}
-              {group.key}
+              {this.columnsMap[this.groupBy].render ?
+                this.tdRender(this.groupBy, group.currentRows[0]) :
+                group.key}
             </span>
             <span className="text-muted">{' '}({group.rows.length})</span>
           </td>
