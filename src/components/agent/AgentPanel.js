96a379ebe138a8ac6afef8558081306ef2cc53a0
Alexander Turivnoi <a.turivnoi@yandex.ru>
2016-11-30 13:10:02 +0300

    AgentPanel:  download-agent url dependent from host

--- src/components/agent/AgentPanel.js
+++ src/components/agent/AgentPanel.js
@@ -3,6 +3,10 @@ import React, {Component, PropTypes} from 'react';
 export default class AgentPanel extends Component {
 
   render() {
+    let agentURL = location.protocol + '//' + location.hostname + (location.port ? ':' + location.port : '') + "/res/agent/dockmaster-agent.py";
+    if (location.hostname === 'localhost') {
+      agentURL = "http://hb1.codeabovelab.com/res/agent/dockmaster-agent.py";
+    }
     return (
       <div>
         <ul className="breadcrumb">
@@ -10,7 +14,7 @@ export default class AgentPanel extends Component {
         </ul>
         <div className="agentList">
           <p>Get Agent:
-            &nbsp;<a href="http://hb1.codeabovelab.com/res/agent/dockmaster-agent.py">http://hb1.codeabovelab.com/res/agent/dockmaster-agent.py</a>
+            &nbsp;<a href={agentURL}>{agentURL}</a>
           </p>
         </div>
       </div>
