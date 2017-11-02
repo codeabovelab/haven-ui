a0e025109e10e6307fc9cf79311ec774aa651de1
Alexander Turivnoi <a.turivnoi@yandex.ru>
2017-11-02 11:08:40 +0300

    DashboardNodes: fixed displaying top 5 Network usage & top 5 memory usage

--- src/components/nodes/DashboardNodesList/DashboardNodesList.js
+++ src/components/nodes/DashboardNodesList/DashboardNodesList.js
@@ -2,6 +2,7 @@ import React, {Component, PropTypes} from 'react';
 import {Link} from 'react-router';
 import {DockTable, OnOff} from '../../index';
 import {Label, Badge, ButtonToolbar, SplitButton, MenuItem, Panel, Button, ProgressBar, Glyphicon} from 'react-bootstrap';
+import _ from 'lodash';
 
 export default class DashboardNodesList extends Component {
   static propTypes = {
@@ -59,13 +60,20 @@ export default class DashboardNodesList extends Component {
     let metricValue = node.health[this.props.metric];
 
     if (this.props.metric === "sysMemUsed") {
-      metricValue = (node.health[this.props.metric] / 1024 / 1024 / 1024).toFixed(1);
+      metricValue = (node.health[this.props.metric] / 1024 / 1024 / 1024).toFixed(2);
       metricValue = `${metricValue} GB`;
     }
 
     if (this.props.metric === "networkIO") {
-      let metricValueIn = (node.health.netIn / 1024 / 1024 / 1024).toFixed(1);
-      let metricValueOut = (node.health.netOut / 1024 / 1024 / 1024).toFixed(1);
+      const nets = node.health.net;
+      let bytesIn = 0;
+      let bytesOut = 0;
+      _.forEach(nets, (el) => {
+        bytesIn += _.get(el, 'bytesIn', 0);
+        bytesOut += _.get(el, 'bytesOut', 0);
+      });
+      let metricValueIn = (bytesIn / 1024 / 1024 / 1024).toFixed(1);
+      let metricValueOut = (bytesOut / 1024 / 1024 / 1024).toFixed(1);
       metricValue = `${metricValueIn}G/${metricValueOut}G`;
     }
 
